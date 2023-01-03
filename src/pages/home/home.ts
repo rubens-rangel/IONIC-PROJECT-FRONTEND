7c3094eaa0c1b469ccab2a1e69d1a7a99ce6ea97
Rubens Rangel <rubens.rangel@b2wdigital.com>
2023-01-03 11:15:08 -0300

    Categorias

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -9,18 +10,19 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
+
   }
 
   ionViewWillEnter() {
-  this.menu.swipeEnable(false);
-  }
-  
-  ionViewDidLeave() {
-  this.menu.swipeEnable(true);
+    this.menu.swipeEnable(false);
   }
 
-  login(){
-    this.navCtrl.push('CategoriasPage') 
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  login() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
